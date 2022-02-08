78174c783b831a9baa732f9c120b9fdb57f0fb47
jocdwardo25 <jocdwardo25@gmail.com>
2022-02-07 19:03:08 -0500

    prueba

---
+++ abc.js
@@ -0,0 +1,20 @@
+const arr = [2, "colors"];
+
+for (let i = 0; i <= arr.length; i++) {
+  const red = () => {};
+}
+const arr = [2, "colors"];
+
+for (let i = 0; i <= arr.length; i++) {
+  const red = () => {};
+}
+const arr = [2, "colors"];
+
+for (let i = 0; i <= arr.length; i++) {
+  const red = () => {};
+}
+const arr = [2, "colors"];
+
+for (let i = 0; i <= arr.length; i++) {
+  const red = () => {};
+}
